ef8e0a73f7d733b4bfb257eb6e70ffecbc5313d3
Ignacio Baixas <ignacio@platan.us>
2020-10-01 10:57:36 -0300

    feat(): implement authentication (signing)

--- authentication.js
+++ authentication.js
@@ -1,12 +1,15 @@
 'use strict';
 
+const querystring = require('querystring');
+const crypto = require('crypto');
+
 // You want to make a request to an endpoint that is either specifically designed
 // to test auth, or one that every user will have access to. eg: `/me`.
 // By returning the entire request object, you have access to the request and
 // response data for testing purposes. Your connection label can access any data
 // from the returned response using the `json.` prefix. eg: `{{json.username}}`.
 const test = (z, bundle) =>
-  z.request({ url: 'https://auth-json-server.zapier-staging.com/me' });
+  z.request({ url: 'https://stg.buda.com/api/v2/me' });
 
 // This function runs after every outbound request. You can use it to check for
 // errors or modify the response. You can have as many as you need. They'll need
@@ -24,17 +27,37 @@ const handleBadResponses = (response, z, bundle) => {
   return response;
 };
 
+const buildMessageToSign = (request, nonce) => {
+  if (request.params) {
+    const splitter = request.url.includes('?') ? '&' : '?';
+    const stringifiedParams = querystring.stringify(request.params);
+
+    if (stringifiedParams) request.url += `${splitter}${stringifiedParams}`;
+  }
+
+  const url = request.url.replace(/^.*\/\/[^\/]+(:\d+)?/i, '');
+
+  if (request.body) {
+    request.body = JSON.stringify(request.body);
+    request.headers['Content-Type'] = 'application/json; charset=utf-8';
+
+    let base64EncodedBody = Buffer.from(request.body).toString('base64');
+    return `${request.method} ${url} ${base64_encoded_body} ${nonce}`;
+  } else {
+    return `${request.method} ${url} ${nonce}`;
+  }
+}
+
 // This function runs before every outbound request. You can have as many as you
 // need. They'll need to each be registered in your index.js file.
 const includeApiKey = (request, z, bundle) => {
-  if (bundle.authData.apiKey) {
-    // Use these lines to include the API key in the querystring
-    request.params = request.params || {};
-    request.params.api_key = bundle.authData.apiKey;
-
-    // If you want to include the API key in the header instead, uncomment this:
-    // request.headers.Authorization = bundle.authData.apiKey;
-  }
+  const nonce = new Date().getTime();
+  const msg = buildMessageToSign(request, nonce);
+  const sign = crypto.createHmac('sha384', bundle.authData.apiSecret).update(msg).digest('hex');
+      
+  request.headers['X-SBTC-APIKEY'] = bundle.authData.apiKey;
+  request.headers['X-SBTC-NONCE'] = nonce;
+  request.headers['X-SBTC-SIGNATURE'] = sign;
 
   return request;
 };
@@ -47,7 +70,10 @@ module.exports = {
 
     // Define any input app's auth requires here. The user will be prompted to enter
     // this info when they connect their account.
-    fields: [{ key: 'apiKey', label: 'API Key', required: true }],
+    fields: [
+      { key: 'apiKey', label: 'API Key', required: true },
+      { key: 'apiSecret', label: 'API Key Secret', required: true }
+    ],
 
     // The test method allows Zapier to verify that the credentials a user provides
     // are valid. We'll execute this method whenver a user connects their account for
